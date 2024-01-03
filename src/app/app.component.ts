118965ad519406947fa55541365e8a8a2bfb9aaf
Andressa Silva <code.andressa@gmail.com>
2024-01-03 17:53:39 -0300

    Added the two inputs and the calculator image.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,5 +12,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-calculator';
+  title = 'My Calculator';
 }
