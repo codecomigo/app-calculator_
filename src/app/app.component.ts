b71270a398a4c63aa4fa02375e95374da13cb32c
Andressa Silva <code.andressa@gmail.com>
2024-01-04 15:51:13 -0300

    Functions added and application completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { TEXTS } from '../../text.js';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
+
 export class AppComponent {
+
   text = TEXTS;
-  
+
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+  result: number = 0;
+
+  addNumbers() {
+    this.result = this.firstNumber + this.secondNumber;
+  }
+
+  subtractNumbers(){
+    this.result = this.firstNumber - this.secondNumber;
+  }
+
+  multiplyNumbers(){
+    this.result =  this.firstNumber * this.secondNumber;
+  }
+
+  divideNumbers(){
+    this.result = this.firstNumber / this.secondNumber;
+  }
+
+
 
 }
